218a9d187d00295141da7ef88d3a6da5c836fcfc
monster-site <133408758+monster-site@users.noreply.github.com>
2023-05-19 11:41:50 -0400

    Update item.js

--- shop/item.js
+++ shop/item.js
@@ -1,1 +1,14 @@
-
+export class Item {
+  constructor(image, text) {
+    this.image = image;
+    this.text = text;
+  }
+  
+  getImage() {
+    return this.image;
+  }
+  
+  getText() {
+    return this.text;
+  }
+}
