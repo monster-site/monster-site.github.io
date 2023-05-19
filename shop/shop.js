03fc23eb9af9f47bb561fbe27fbf5d1ce68e1353
monster-site <133408758+monster-site@users.noreply.github.com>
2023-05-19 12:43:04 -0400

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -1,3 +1,12 @@
+const data = [
+  {name: "Item 1", image: "branch-logo.png", text: "Details for item 1", price: "$10"},
+  {name: "Item 2", image: "branch-logo.png", text: "Details for item 2", price: "$20"},
+  {name: "Item 3", image: "branch-logo.png", text: "Details for item 3", price: "$30"},
+  {name: "Item 4", image: "branch-logo.png", text: "Details for item 4", price: "$40"},
+  {name: "Item 5", image: "branch-logo.png", text: "Details for item 5", price: "$50"},
+  {name: "Item 6", image: "branch-logo.png", text: "Details for item 6", price: "$60"},
+];
+
 var shopItemIds = ["shop-item-1", "shop-item-2", "shop-item-3", "shop-item-4", "shop-item-5", "shop-item-6"];
 
 loadShopItems() {
