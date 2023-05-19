9c966edec7be6a6cab16d90dcd361422c8ad85e1
monster-site <133408758+monster-site@users.noreply.github.com>
2023-05-19 11:49:36 -0400

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -1,1 +1,7 @@
+var shopItemImages = ["branch-logo.png", "branch-logo.png", "branch-logo.png", "branch-logo.png", "branch-logo.png"];
 
+loadShopItems() {
+  for(var i = 0; i < shopItemImages.length; i++) {
+    
+  }
+}
