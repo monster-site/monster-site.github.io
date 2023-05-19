641f286914174b7feceb3864853b1c4d554723ad
monster-site <133408758+monster-site@users.noreply.github.com>
2023-05-19 12:31:51 -0400

    Update data.js

--- shop/data.js
+++ shop/data.js
@@ -1,1 +1,8 @@
-
+const data = [
+  {name: "Item 1", image: "branch-logo.png", text: "Details for item 1", price: "$10"},
+  {name: "Item 2", image: "branch-logo.png", text: "Details for item 2", price: "$20"},
+  {name: "Item 3", image: "branch-logo.png", text: "Details for item 3", price: "$30"},
+  {name: "Item 4", image: "branch-logo.png", text: "Details for item 4", price: "$40"},
+  {name: "Item 5", image: "branch-logo.png", text: "Details for item 5", price: "$50"},
+  {name: "Item 6", image: "branch-logo.png", text: "Details for item 6", price: "$60"},
+];
