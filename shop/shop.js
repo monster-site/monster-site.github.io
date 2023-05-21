d6d7fa1fd75c324797c07566e2123243be67e2b6
monster-site <133408758+monster-site@users.noreply.github.com>
2023-05-21 18:32:28 -0400

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -9,7 +9,7 @@ const data = [
 
 var shopItemIds = ["shop-item-1", "shop-item-2", "shop-item-3", "shop-item-4", "shop-item-5", "shop-item-6"];
 
-loadShopItems() {
+function loadShopItems() {
   for(var i = 0; i < shopItemIds.length; i++) {
     const name = data[i]["name"];
     const image = data[i]["image"];
