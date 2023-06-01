52c564b406ad747c481f8a01f991918b18087a76
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-01 18:42:55 -0400

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -1,12 +1,12 @@
 const data = [
-  {name: "Monster 1", image: "A_blue.png", color: "#d8e8f6"},
-  {name: "Monster 2", image: "B_pink.png", color: "#f0dee6"},
-  {name: "Monster 3", image: "C_green.png", color: "#daeae5"},
-  {name: "Monster 4", image: "D_orange.png", color: "#f2e7de"},
-  {name: "Monster 5", image: "E_teal.png", color: "#d1eff6"},
-  {name: "Monster 6", image: "F_purple.png", color: "#dfd4e8"},
-  {name: "Monster 7", image: "G_yellow.png", color: "#f5f1df"},
-  {name: "Monster 8", image: "H_red.png", color: "#e3d3d8"}
+  {name: "Monster 1", image: "A_blue.png", color: "#d8e8f6", textcolor: "#18689c"},
+  {name: "Monster 2", image: "B_pink.png", color: "#f0dee6", textcolor: "#9d172a"},
+  {name: "Monster 3", image: "C_green.png", color: "#daeae5", textcolor: "#1a6a37"},
+  {name: "Monster 4", image: "D_orange.png", color: "#f2e7de", textcolor: "#90571a"},
+  {name: "Monster 5", image: "E_teal.png", color: "#d1eff6", textcolor: "#00b4c5"},
+  {name: "Monster 6", image: "F_purple.png", color: "#dfd4e8", textcolor: "#80378d"},
+  {name: "Monster 7", image: "G_yellow.png", color: "#f5f1df", textcolor: "#a08614"},
+  {name: "Monster 8", image: "H_red.png", color: "#e3d3d8", textcolor: "#912629"}
 ];
 
 function loadImage(id, index) {
