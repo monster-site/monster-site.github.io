35a38d8bac9543ad9963f6eac9d37da270887035
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-02 13:58:02 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -5,17 +5,20 @@ function handleAccountCreated() {
   name = document.getElementById("create-name").value;
   username = document.getElementById("create-username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
-  document.getElementById("profile-header-text").value = "Hello, " + name + "!";
-  //set username
-  //show detail view div
-  //set the hello name text
-  //set the username text
 }
 
 function handleUserLoggedIn() {
-  //hide the login div
-  //set name and password to username
-  //show detail view div
-  //set the hello name text
-  //set the username text
+  username = document.getElementById("username").value;
+  window.location.href = "https://monster-site.github.io/profile/user.html";
 }
+
+function onProfileLoaded() {
+  if(name) {
+    document.getElementById("profile-header-text").value = "Hello, " + name + "!";
+  } else {
+    document.getElementById("profile-header-text").value = "Hello, " + username + "!";
+  }
+   document.getElementById("username").value = username;
+}
+
+addEventListener("DOMContentLoaded", (event) => {onProfileLoaded();});
