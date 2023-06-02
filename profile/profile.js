fdeabd904f43484405816c38b4e470abdb5c48be
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-02 11:40:50 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -1,21 +1,19 @@
 var name = "";
 var username = "";
 
-function showDetailView() {
+function handleAccountCreated() {
+  //hide the create account div
+  //set name
+  //set password
   //show detail view div
   //set the hello name text
   //set the username text
 }
 
-function handleAccountCreated() {
-  //hide the create account div
-  //set name
-  //set password
-  showDetailView();
-}
-
 function handleUserLoggedIn() {
   //hide the login div
   //set name and password to username
-  showDetailView();
+  //show detail view div
+  //set the hello name text
+  //set the username text
 }
