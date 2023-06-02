18c2b18316002cccf99f6210637de2d4fac2b301
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-02 15:21:30 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -1,31 +1,28 @@
-var name = "";
-var username = "";
-
 function handleAccountCreated() {
-  name = document.getElementById("create-name").value;
-  username = document.getElementById("create-username").value;
+  window.name = document.getElementById("create-name").value;
+  window.username = document.getElementById("create-username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
   return false;
 }
 
 function handleUserLoggedIn() {
-  username = document.getElementById("username").value;
+  window.username = document.getElementById("username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
   return false;
 }
 
 function onProfileLoaded() {
   console.log("hit");
-  if(!name && !username) {
+  if(!window.name && !window.username) {
     console.log("hit 1");
     return;
   }
   else if(name) {
-    document.getElementById("profile-header-text").value = "Hello, " + name + "!";
+    document.getElementById("profile-header-text").value = "Hello, " + window.name + "!";
   } else {
-    document.getElementById("profile-header-text").value = "Hello, " + username + "!";
+    document.getElementById("profile-header-text").value = "Hello, " + window.username + "!";
   }
-   document.getElementById("username").value = username;
+   document.getElementById("username").value = window.username;
 }
 
 addEventListener("DOMContentLoaded", (event) => {onProfileLoaded();});
