9ac2c1600556d0eba4befe6638bd088b7abe6dd1
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-02 12:29:15 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -5,9 +5,8 @@ function handleAccountCreated() {
   name = document.getElementById("create-name").value;
   username = document.getElementById("create-username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
-  
-  //set name
-  //set password
+  document.getElementById("profile-header-text").value = "Hello, " + name + "!";
+  //set username
   //show detail view div
   //set the hello name text
   //set the username text
