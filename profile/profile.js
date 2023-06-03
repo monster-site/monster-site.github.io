e6cf814eee0b64b2e1512b22ea399162dd0ea828
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-03 19:04:03 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -2,27 +2,33 @@ function handleAccountCreated() {
   window.name = document.getElementById("create-name").value;
   window.username = document.getElementById("create-username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
-  console.log(window.name);
-  console.log(window.username);
   return false;
 }
 
 function handleUserLoggedIn() {
   window.username = document.getElementById("username").value;
   window.location.href = "https://monster-site.github.io/profile/user.html";
-  console.log(window.username);
   return false;
 }
 
+function showSignedInView() {
+  document.getElementById("signed-in").hidden = false;
+  document.getElementById("signed-out").hidden = true;
+}
+
 function onProfileLoaded() {
+  console.log("name: " + window.name);
+  console.log("username: " + window.username);
   if(!window.name && !window.username) {
     return;
   }
   else if(window.name) {
     console.log("1");
+    showSignedInView();
     document.getElementById("profile-header-text").value = "Hello, " + window.name + "!";
   } else {
     console.log("2");
+    showSignedInView();
     document.getElementById("profile-header-text").value = "Hello, " + window.username + "!";
   }
    document.getElementById("username").value = window.username;
