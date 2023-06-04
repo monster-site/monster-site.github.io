1d2ccad7bd097211fb4ec3f5585185ca6f98dae8
monster-site <133408758+monster-site@users.noreply.github.com>
2023-06-03 20:11:33 -0400

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -1,16 +1,16 @@
 function handleAccountCreated() {
   window.name = document.getElementById("create-name").value;
-  window.username = document.getElementById("create-username").value;
+  window.name2 = document.getElementById("create-username").value;
   console.log("name: " + window.name);
-  console.log("username: " + window.username);
+  console.log("username: " + window.name2);
   window.location.href = "https://monster-site.github.io/profile/user.html";
   return false;
 }
 
 function handleUserLoggedIn() {
-  window.username = document.getElementById("login-username").value;
+  window.name2 = document.getElementById("login-username").value;
   console.log("name: " + window.name);
-  console.log("username: " + window.username);
+  console.log("username: " + window.name2);
   window.location.href = "https://monster-site.github.io/profile/user.html";
   return false;
 }
@@ -22,7 +22,7 @@ function showSignedInView() {
 
 function logout() {
   window.name = "";
-  window.username = "";
+  window.name2 = "";
   document.getElementById("signed-in").hidden = true;
   document.getElementById("signed-out").hidden = false;
   window.location.href = "https://monster-site.github.io/profile/user.html";
@@ -31,8 +31,8 @@ function logout() {
 
 function onProfileLoaded() {
   console.log("name: " + window.name);
-  console.log("username: " + window.username);
-  if(!window.name && !window.username) {
+  console.log("username: " + window.name2);
+  if(!window.name && !window.name2) {
     return;
   }
   else if(window.name) {
@@ -42,7 +42,7 @@ function onProfileLoaded() {
   } else {
     console.log("2");
     showSignedInView();
-    document.getElementById("profile-header-text").value = "Hello, " + window.username + "!";
+    document.getElementById("profile-header-text").value = "Hello, " + window.name2 + "!";
   }
 }
 
