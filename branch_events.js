35e0af97ce7d463d920c13b138568f3f6afbeda6
amedina-branch <130110448+amedina-branch@users.noreply.github.com>
2023-09-01 11:03:19 -0700

    Test currency and revenue fields

--- branch_events.js
+++ branch_events.js
@@ -1,4 +1,21 @@
 function trackEvent(eventName) {
-  branch.logEvent(eventName);
+  if (eventName == "ADD_TO_CART") {
+    var event_and_custom_data = {
+      "currency": "USD",
+      "revenue": 120.0
+    };
+
+    var customer_event_alias = "Test currency + revenue fields";
+
+    branch.logEvent(
+      "ADD_TO_CART",
+      event_and_custom_data,
+      customer_event_alias,
+      function(err) { console.log(err); }
+    );
+  } else {
+    branch.logEvent(eventName);
+  }
+
   setTimeout(function(){alert("Branch " + eventName + " event was tracked!")},500);
 }
