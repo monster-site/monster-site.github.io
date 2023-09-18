239231fb8fda53fba29665caf08314f1635ea734
monster-site <133408758+monster-site@users.noreply.github.com>
2023-09-18 15:24:57 -0400

    Update branch_events.js

--- branch_events.js
+++ branch_events.js
@@ -1,5 +1,5 @@
 function trackEvent(eventName) {
-  if (eventName == "ADD_TO_CART" || eventName == "PURCHASE") {
+  if (eventName == "ADD_TO_CART") {
     var event_and_custom_data = {
       "currency": "EUR",
       "revenue": 120.0
@@ -15,8 +15,26 @@ function trackEvent(eventName) {
     );
 
     setTimeout(function(){alert("Branch " + eventName + " event was tracked with currency and revenue data!")},500);
+  } 
+  else if(eventName == "PURCHASE") {
+    var event_and_custom_data = {
+      "currency": "EUR",
+      "revenue": 120.0
+    };
 
-  } else {
+    var customer_event_alias = "Test currency + revenue fields";
+
+    branch.logEvent(
+      "PURCHASE",
+      event_and_custom_data,
+      customer_event_alias,
+      function(err) { console.log(err); }
+    );
+
+    setTimeout(function(){alert("Branch " + eventName + " event was tracked with currency and revenue data!")},500);
+
+  }
+  else {
     branch.logEvent(eventName);
 
     setTimeout(function(){alert("Branch " + eventName + " event was tracked!")},500);
